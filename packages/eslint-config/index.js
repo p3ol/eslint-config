22f8d120f4ef01b3b5e468baaee1c84b1f4ad519
Ugo Stephant <ugo@poool.fr>
2020-06-12 17:41:57 +0200

    fix(config): avoid reporting arrow parens when there's only one arg

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -44,6 +44,7 @@ module.exports = {
     'object-curly-newline': OFF,
     'object-property-newline': OFF,
     'no-empty': [ERROR, { allowEmptyCatch: true }],
+    'arrow-parens': [WARNING, 'as-needed'],
 
     // ES6 fixes
     camelcase: OFF,
