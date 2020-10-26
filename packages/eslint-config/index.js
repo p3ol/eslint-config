b98fa3d59591f53bbf7c09466d8619ae3ed6f692
Ugo Stephant <ugo@poool.fr>
2020-10-26 15:52:00 +0100

    fix(config): fix config standard v15 offsetTernaryExpressions option

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -46,6 +46,22 @@ module.exports = {
     'no-empty': [ERROR, { allowEmptyCatch: true }],
     'arrow-parens': [WARNING, 'as-needed'],
     'multiline-ternary': OFF,
+    indent: [ERROR, 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1, body: 1 },
+      FunctionExpression: { parameters: 1, body: 1 },
+      CallExpression: { arguments: 1 },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoreComments: false,
+      ignoredNodes: ['TemplateLiteral *'],
+      offsetTernaryExpressions: false,
+    }],
 
     // ES6 fixes
     camelcase: OFF,
