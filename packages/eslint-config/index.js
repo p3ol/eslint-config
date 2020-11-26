c597d5cd56162686bb7a2c08b8745b244ec1b87e
Ugo Stephant <ugo@poool.fr>
2020-11-26 17:23:12 +0100

    fix(config): normalize import/order rule

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -88,19 +88,13 @@ module.exports = {
     // Import
     'import/newline-after-import': WARNING,
     'import/order': [WARNING, {
-      groups: ['builtin', 'external', ['parent', 'sibling', 'index']],
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling', 'index', 'unknown'],
+      ],
       'newlines-between': 'always',
-      pathGroups: [{
-        pattern: '*.{styl,css}',
-        patternOptions: { matchBase: true, nocase: true },
-        group: 'index',
-        position: 'after',
-      }, {
-        pattern: '*.{svg,png,jpeg,jpg,gif}',
-        patternOptions: { matchBase: true, nocase: true },
-        group: 'index',
-        position: 'after',
-      }],
     }],
 
     // Custom rules
