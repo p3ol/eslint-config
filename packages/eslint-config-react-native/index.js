b001f9c527b16e00662764bf6b9d36e6164665cf
Ugo Stephant <ugo@poool.fr>
2020-11-26 17:23:49 +0100

    fix(config-react-native): adapt import/order rule for react-native

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -27,5 +27,27 @@ module.exports = {
   },
   rules: {
     'react/display-name': WARNING,
+
+    // import
+    'import/order': [WARNING, {
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling', 'index', 'unknown'],
+      ],
+      'newlines-between': 'always',
+      pathGroups: [{
+        pattern: 'styles',
+        patternOptions: { matchBase: true, nocase: true },
+        group: 'index',
+        position: 'after',
+      }, {
+        pattern: '*.{svg,png,jpeg,jpg,gif}',
+        patternOptions: { matchBase: true, nocase: true },
+        group: 'index',
+        position: 'after',
+      }],
+    }],
   },
 };
